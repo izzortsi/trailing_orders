4725aa5ab5ed9e91c9b440460d4c60eed712c05f
Igor Strozzi <istrozzi@matematica.ufrj.br>
2022-05-13 11:53:45 -0300

    ringbuffer for cklines with indicators; liveplotting

--- dev/ringbuffer_ws-cklines-indicators.py
+++ dev/ringbuffer_ws-cklines-indicators.py
@@ -1,6 +1,8 @@
 
 # %%
-
+%matplotlib qt
+import matplotlib.pyplot as plt
+import numpy as np
 import time
 import logging
 from binance.lib.utils import config_logging
@@ -9,12 +11,14 @@ from binance.websocket.futures.websocket_client import FuturesWebsocketClient as
 config_logging(logging, logging.DEBUG)
 from binance.websocket.futures.websocket_client import FuturesWebsocketClient
 import pandas as pd
+import pandas_ta as ta
+
 
 class RingBuffer(FuturesWebsocketClient):
     def __init__(self, size):
         self.df = pd.DataFrame()
         self.size = size
-
+        self.indicators = pd.DataFrame()
         super().__init__()
         
 
@@ -57,6 +61,28 @@ class RingBuffer(FuturesWebsocketClient):
                             ])], 
                             ignore_index = True,
                         )
+                atr_ = ta.atr(self.df.h, self.df.l, self.df.c, length=24)                        
+                # print(atr_)
+                closes_ema = ta.ema(self.df.c, length=24)
+                closes_ema.name = "ema"
+                # print(closes_ema)
+                sup_band = closes_ema + 0.84*atr_
+                sup_band.name = "sband"
+                # print(sup_band)
+                inf_band = closes_ema - 0.84*atr_
+                inf_band.name = "iband"
+                # indicators_row = pd.concat([inf_band, closes_ema, sup_band], axis=1)
+                # print(indicators_row)
+                # self.indicators_df = pd.concat([
+                #                                 self.indicators_df,
+                #                                 indicators_row
+                #                                 ],
+                #                                 ignore_index=True,
+                #                             )
+                self.indicators = pd.concat([inf_band, closes_ema, sup_band], axis=1)
+                # print(self.indicators)
+                # self.indicators_df = ta.atr(self.df.h, self.df.l, self.df.c, timeperiod=6)
+                # print(self.indicators_df.iloc[-1])
                 # print(self.df[["date", "c"]].iloc[0])
                 # print(self.df[["date", "c"]].iloc[-1])
                 # print(self.df.date.iloc[-1] - self.df.date.iloc[0])
@@ -70,10 +96,35 @@ b.continuous_kline(
     pair="btcusdt",
     id=1,
     contractType="perpetual", 
-    interval='4h',
+    interval='1m',
     callback=b.message_handler,
 )
 
+#%%
+f, ax = plt.subplots(figsize=(12, 8))
+# ax.set_xlim(0, 255)
+
+# ax.set_ylim(0, 500)
+
 # %%
 
-b.stop()
+ax.plot(b.df.date, b.df.c, label="close")
+ax.plot(b.df.date, b.indicators.ema)
+ax.plot(b.df.date, b.indicators.iband)
+ax.plot(b.df.date, b.indicators.sband)
+# %%
+ax.clear()
+# %%
+
+# ax.lines[0].set_data(b.df.date, b.df.c)
+# ax.lines[0].set_data(b.df.date, b.indicators.ema)
+# ax.lines[2].set_data(b.df.date, b.indicators.iband)
+# ax.lines[3].set_data(b.df.date, b.indicators.sband)
+# #%%
+
+# #%%
+# ax.lines[0].draw(ax.plot(b.df.date, b.df.c))
+# ax.lines[1].draw(ax.plot(b.df.date,b.indicators.ema))
+# ax.lines[2].draw(ax.plot(b.df.date,b.indicators.iband))
+# ax.lines[3].draw(ax.plot(b.df.date,b.indicators.sband))
+#%%
